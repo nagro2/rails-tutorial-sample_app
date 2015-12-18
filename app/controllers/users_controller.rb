502565cd4205cbfe6753738e91e1ac659b56fb59
nagro <nick_agro@hotmail.com>
2015-12-18 12:33:25 -0500

    Prior to AWS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     #@user = User.find(1)
     #debugger
   end
@@ -62,13 +63,13 @@ class UsersController < ApplicationController
     # Before filters
 
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location # store_location, defined in sessions_helper.rb, stores url that the user was trying to go to
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+#    def logged_in_user
+#      unless logged_in?
+#        store_location # store_location, defined in sessions_helper.rb, stores url that the user was trying to go to
+#        flash[:danger] = "Please log in."
+#        redirect_to login_url
+#      end
+#    end
 
     # Confirms the correct user.
     def correct_user
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   #resources :users
+  #resources :account_activations, only: [:edit]
+  #resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+
 
 #  get 'sessions/new'
 #  get 'users/new'
